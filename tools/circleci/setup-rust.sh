d049bc5d7d961f20ba97ef48cc59b497ad708c2d
Tatsuya Kawano <tatsuya@hibaridb.org>
2016-06-04 14:27:45 +0800

    Update circle.yml to install Rust and rustbook and build the book

---
+++ tools/circleci/setup-rust.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+set -u
+
+$RUST_HOME=$1
+$RUST_NIGHTLY_RELEASE_DATE=$2
+$VERSION="nightly-$RUST_NIGHTLY_RELEASE_DATE"
+
+if [ -d $RUST_HOME ]; then
+  echo "Using Rust version $VERSION"
+else
+  echo "Installing Rust $VERSION using rustup.sh"
+  curl -sSf https://static.rust-lang.org/rustup.sh | \
+       sh -s -- --prefix=$RUST_HOME --channel=nightly --$RUST_NIGHTLY_RELEASE_DATE --disable-sudo
+fi
