18a3d615f75595043a545fefd0a68b583ea1d3d0
Tatsuya Kawano <tatsuya@hibaridb.org>
2016-06-04 15:05:52 +0800

    Add more debug prints to circle.yml and setup-rust.sh

--- tools/circleci/setup-rust.sh
+++ tools/circleci/setup-rust.sh
@@ -8,9 +8,9 @@ RUST_NIGHTLY_RELEASE_DATE=$2
 VERSION="nightly-$RUST_NIGHTLY_RELEASE_DATE"
 
 if [ -d $RUST_HOME ]; then
-  echo "Using Rust version $VERSION"
+  echo "Using Rust version $VERSION at $RUST_HOME"
 else
-  echo "Installing Rust $VERSION using rustup.sh"
+  echo "Installing Rust $VERSION to $RUST_HOME using rustup.sh"
   curl -sSf https://static.rust-lang.org/rustup.sh | \
-       sh -s -- --prefix=$RUST_HOME --channel=nightly --$RUST_NIGHTLY_RELEASE_DATE --disable-sudo
+      sh -s -- --prefix=$RUST_HOME --channel=nightly --$RUST_NIGHTLY_RELEASE_DATE --disable-sudo
 fi
